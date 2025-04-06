aac7a14516d413ef863f81e3c0182a69bbe26dab
aliMobasshir <mobasshir0001@gmail.com>
2025-04-06 20:01:15 +0530

    the final

--- src/components/Logo/logo.jsx
+++ src/components/Logo/logo.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/Logo.png'
 
 function Logo({width="100px"}) {
   return (
-    <div className='flex items-center gap-1 text-amber-50 font-["Codystar"] [font-size:clamp(1.3rem,3vw,2rem)] whitespace-nowrap'>
+    <div className='flex items-center gap-1 text-amber-50 font-["Codystar"] [font-size:clamp(1.5rem,3vw,2rem)] whitespace-nowrap'>
       <img src={logo} style={{width: width}} className='mt-2 min-w-[40px] w-[75px] md:w-[75px]' alt="Logo" />
       Cashew AI
     </div>
