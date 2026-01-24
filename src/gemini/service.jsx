8f055ee60c7b1afba841af4a227777395a786fc2
MD Mobasshir Ali <168336051+aliMobasshir@users.noreply.github.com>
2026-01-24 16:36:27 +0530

    Update AI model version from 2.0 to 2.5

--- src/gemini/service.jsx
+++ src/gemini/service.jsx
@@ -8,7 +8,7 @@ export class Service {
     async generateContent(conversationHistory)  {
         try {
           const response = await ai.models.generateContent({
-            model: "gemini-2.0-flash",
+            model: "gemini-2.5-flash",
             contents: conversationHistory,
           });
           return response.text;
